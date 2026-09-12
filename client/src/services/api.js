e4d3e78481c3a9ce90d18fd043f8e9ed2354c10e
PraveenRaii <praveen1234202040@gmail.com>
2026-09-12 14:16:14 +0530

    change

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://parking-booking-system-pe0t.onrender.com"
 });
 
 API.interceptors.request.use(
